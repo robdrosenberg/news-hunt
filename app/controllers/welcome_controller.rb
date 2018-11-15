4123b23905b45a7db7af949cebc3c5bee1514278
Robert Rosenberg <robertdr85@gmail.com>
2018-11-15 14:32:25 -0800

    fix welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    render :file Rails.root.join('views', 'welcome', 'index.html.erb')
+    render file: Rails.root.join('views', 'welcome', 'index.html.erb')
   end
 end
