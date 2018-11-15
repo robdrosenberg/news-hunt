6d5660eda8abb428396f9a2ee1bda71fd659b253
Robert Rosenberg <robertdr85@gmail.com>
2018-11-15 14:50:05 -0800

    Fix Root.join

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    render file: Rails.root.join('public','index.html')
+    render file: Rails.root.join('..', 'public', 'index.html')
   end
 end
